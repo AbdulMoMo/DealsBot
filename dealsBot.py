8a80a1d3a651a1d7d4b941d006c80a8e9a36c03d
Abdul Mohamed <your-email-address>
2022-09-06 01:29:39 -0700

    Begin decoupling hunting logic

--- dealsBot.py
+++ dealsBot.py
@@ -7,7 +7,7 @@ rClientId = tokenData['reddit']['client_id']
 rClientSecret = tokenData['reddit']['client_secret']
 rUserAgent = tokenData['reddit']['user_agent']
 
-class hunter(discord.Client):
+class discord_listner(discord.Client):
 
     commandToCall = None
     dealFinder = praw.Reddit(client_id=rClientId,
@@ -39,7 +39,7 @@ class hunter(discord.Client):
     async def on_ready(self):
         channel = self.get_channel(1012892793582129213)
         await channel.send("!hotdeals")
-        await channel.send(datetime.datetime.now())
+        #await channel.send(datetime.datetime.now())
 
 
     async def on_message(self, message):
@@ -52,12 +52,44 @@ class hunter(discord.Client):
         for post in result: #type: str
             await message.channel.send(post)
 
+# Reddit client to decouple deal-hunting logic (WIP)
+class reddit_hunter:
+
+    dealFinder = None
+    commandToCall = None
+
+    def __init__(self, subreddit):
+
+        self.dealFinder = praw.Reddit(client_id=rClientId,
+                                  client_secret=rClientSecret,
+                                  user_agent=rUserAgent)
+
+        sub: praw.models.SubredditHelper = self.dealFinder.subreddit(subreddit)
+        self.commandToCall = {
+            "!hotdeals": self._get_game_deals_func(sub.hot),
+            "!risingdeals": self._get_game_deals_func(sub.rising),
+            "!topdeals": self._get_game_deals_func(sub.top),
+            "!controversialdeals": self._get_game_deals_func(sub.controversial)
+        }
+
+    def _get_game_deals(self, count: int, method) -> list[str]:
+        #print("This method was actually called")
+        resultList = [f"{submission.title} : {submission.url}" for submission in method(limit=count)]
+        #print(resultList)
+        return resultList
+
+    def _get_game_deals_func(self, method):
+        return lambda count : self._get_game_deals(count, method)
+
+    def is_valid_action(self, action: str):
+        return action in self.commandToCall.keys()
+
 
 discordHandler = logging.FileHandler(filename='./discord.log', encoding='utf-8', mode='w')
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = hunter(intents=intents)
+client = discord_listner(intents=intents)
 client.run(discordToken, log_handler=discordHandler)
 
 
