b5c8c719a3e4624b070221f4bc170d2e57c6cf65
Abdul Mohamed <your-email-address>
2022-09-09 02:38:15 -0700

    Add channel-level state persistence

--- dealsBot.py
+++ dealsBot.py
@@ -7,52 +7,7 @@ tokenData = json.load(tokenJson)
 discordToken = tokenData['discord']
 rClientId = tokenData['reddit']['client_id']
 rClientSecret = tokenData['reddit']['client_secret']
-rUserAgent = tokenData['reddit']['user_agent']
-
-# class discord_listener(discord.Client):
-
-#     rClient = None
-    
-#     async def on_ready(self):
-#         self.rClient = reddit_hunter("GameDeals")
-
-#     async def on_message(self, message):
-#         raw_message: str = message.content
-        
-#         print(message.guild.name)
-#         if raw_message == "!helpDealzBot" or raw_message.startswith("!help"):
-#             await message.channel.send("Please note the default subreddit is Game Deals. To set a new subreddit please use !select<subreddit_name>.")
-
-#         if raw_message == "!currentsub":
-#             await message.channel.send(f"r/{self.rClient.subreddit}")
-
-#         if raw_message.startswith("!select"):
-#             self._set_rClient(raw_message.split("!select", 1)[1])
-           
-        
-#         command = None
-#         count = 5
-
-#         if raw_message[0] == "!" and raw_message[1].isdigit():
-#             command = raw_message[:1] + raw_message[2:]
-#             count = int(raw_message[1])
-#         else:
-#             command: str = raw_message
-
-#         if not self.rClient.is_valid_action(command):
-#             return
-        
-#         try:
-#             result: list[str] = self.rClient.commandToCall[command](count)
-#         except:
-#             await message.channel.send(f"Please check that r/{self.rClient.subreddit} is spelled correctly and exists. Set again with !select")
-
-#         for post in result: #type: str
-#             await message.channel.send(post)
-    
-#     def _set_rClient(self, subreddit):
-#         self.rClient = reddit_hunter(subreddit)
-        
+rUserAgent = tokenData['reddit']['user_agent']    
 
 # Reddit client to decouple deal-hunting logic
 class reddit_hunter:
@@ -60,63 +15,81 @@ class reddit_hunter:
     dealFinder = praw.Reddit(client_id=rClientId,
                                   client_secret=rClientSecret,
                                   user_agent=rUserAgent)
-    commandToCall = None
 
-    subreddit = None
+    def __init__(self) -> None:
+        self.channelToSub = {}
 
-    channelToSub = None
+    def add_or_get_sub(self, channel, subreddit, isShow):
+        if subreddit != '':
+            self.channelToSub[channel] = self.subreddit_hunter(subreddit, self.dealFinder)
+        elif not isShow:
+            self.channelToSub[channel] = self.subreddit_hunter('GameDeals', self.dealFinder) 
+        return self.channelToSub[channel]
 
-    def __init__(self, subreddit) -> None:
-        self.subreddit = subreddit
-        sub: praw.models.Subreddit = self.dealFinder.subreddit(subreddit)
-        self.commandToCall = {
-            "!hotdeals": self._get_game_deals_func(sub.hot),
-            "!risingdeals": self._get_game_deals_func(sub.rising),
-            "!topdeals": self._get_game_deals_func(sub.top),
-            "!controversialdeals": self._get_game_deals_func(sub.controversial)
-        }
+    class subreddit_hunter():
 
-    def _get_game_deals(self, count: int, method) -> list[str]:
-        resultList = [f"{submission.title} : {submission.url}" for submission in method(limit=count)]
-        return resultList
+        def __init__(self, subreddit, dealFinder) -> None:
+            self.subreddit = subreddit
+            sub: praw.models.Subreddit = dealFinder.subreddit(subreddit)
+            self.commandToCall = {
+                "hotdeals": self._get_game_deals_func(sub.hot),
+                "risingdeals": self._get_game_deals_func(sub.rising),
+                "topdeals": self._get_game_deals_func(sub.top),
+                "controversialdeals": self._get_game_deals_func(sub.controversial)
+            }
 
-    def _get_game_deals_func(self, method):
-        return lambda count : self._get_game_deals(count, method)
+        def _get_game_deals(self, count: int, method) -> list[str]:
+            resultList = [f"{submission.title} : {submission.url}" for submission in method(limit=count)]
+            return resultList
 
-    def is_valid_action(self, action: str):
-        return action in self.commandToCall.keys()
+        def _get_game_deals_func(self, method):
+            return lambda count : self._get_game_deals(count, method)
 
-    # class subreddit_hunter: 
+        def is_valid_action(self, action: str):
+            return action in self.commandToCall.keys()
 
-class redditCommands(commands.Cog):
+
+
+class reddit_commands(commands.Cog):
     
-    rClient = reddit_hunter("GameDeals")
+    rClient = reddit_hunter()
 
     def __init__(self, bot):
         self.bot = bot
 
-    def _set_rClient(self, subreddit):
-        self.rClient = reddit_hunter(subreddit)
+    def _set_or_get_rClient(self, channel, subreddit, isShow):
+        return self.rClient.add_or_get_sub(channel, subreddit, isShow)
     
     @commands.command()
     async def currentsub(self, ctx):
-        await ctx.send(f"r/{self.rClient.subreddit}")
+        channel: str = ctx.channel
+        await ctx.send(f"r/{self.rClient.add_or_get_sub(channel, '', False).subreddit}")
 
     @commands.command()
     async def select(self, ctx, arg):
-        print(arg)
-        self._set_rClient(arg)
-        await ctx.send(f" The subreddit set is now r/{self.rClient.subreddit}")
+        channel: str = ctx.channel
+        self._set_or_get_rClient(channel, arg, False)
+        await ctx.send(f" The subreddit set is now r/{self.rClient.channelToSub[channel].subreddit}")
+    
+    @commands.command()
+    async def show(self, ctx, type):
+        channel: str = ctx.channel
+        sub = self._set_or_get_rClient(ctx.channel, '', True)
+        try:
+            result: list[str] = sub.commandToCall[type](5)
+            # print(result)
+            for post in result: #type: str
+                await ctx.send(post)
+        except:
+            await ctx.send(f"Please check that r/{self.rClient.channelToSub[channel].subreddit} is spelled correctly and exists. Set again with $select")
 
 discordHandler = logging.FileHandler(filename='./discord.log', encoding='utf-8', mode='w')
 intents = discord.Intents.default()
 intents.message_content = True
 
 activity = discord.Activity(name='for them dealz', type=discord.ActivityType.watching)
-# client = discord_listener(intents=intents, activity=activity)
 bot = commands.Bot(command_prefix='$', intents=intents, activity=activity)
 bot.remove_command('help')
-# client.run(discordToken, log_handler=discordHandler)
 
 @bot.command()
 async def help(ctx):
@@ -125,5 +98,5 @@ async def help(ctx):
 async def add_cog(bot, cog):
     await bot.add_cog(cog)
 
-asyncio.run(add_cog(bot, redditCommands(bot)))
+asyncio.run(add_cog(bot, reddit_commands(bot)))
 bot.run(discordToken, log_handler=discordHandler)
