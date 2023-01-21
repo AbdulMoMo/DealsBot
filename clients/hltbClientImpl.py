170ce196946af82d07202e2af60100715f0475a2
Abdul Mohamed <abdulramenmohamed@gmail.com>
2023-01-21 15:50:31 -0800

    Update TODO for HTLB feat

--- clients/hltbClientImpl.py
+++ clients/hltbClientImpl.py
@@ -9,9 +9,11 @@ class hltb_hunter():
     def __init__(self) -> None:
         self.hltbClient = HowLongToBeat()
 
+    # Note: Ref for HTLB Entry: https://github.com/ScrappyCocco/HowLongToBeat-PythonAPI/blob/master/howlongtobeatpy/howlongtobeatpy/HowLongToBeatEntry.py
     def search(self, query: str) -> HowLongToBeat:
         results = self.hltbClient.search(query)
         if results is not None and len(results) > 0:
             best_match: HowLongToBeat = max(results, key=lambda element: element.similarity)
+            # TODO: Sort attr to populate results for making a thread
             best_match_attr = best_match.__dict__.keys()
         return None
